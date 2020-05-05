e21a8692cc99606a69fc215eb11aed991849974f
HarshikaK <harshikakaushani21@gmil.com>
2020-05-05 23:41:26 +0530

    read crud was added

--- PAF_PatientService/src/com/Patient.java
+++ PAF_PatientService/src/com/Patient.java
@@ -2,6 +2,8 @@ package com;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Patient {
 	
@@ -20,5 +22,46 @@ public class Patient {
 			{e.printStackTrace();}
 			return con;
 		} 
+		// reading an Patient -------------------------
+		public String readPatient()
+		{
+			String output = "";
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database for reading.";
+				}
+				// Prepare the html table to be displayed
+				output = "<table border='1'><tr><th>PatientName</th> <th>Email</th><th>Phone</th>"+ "<th>Password</th><th>Update</th><th>Remove</th></tr>";
+				String query = "select * from Patient";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String PID = Integer.toString(rs.getInt("PID"));
+					String PatientName = rs.getString("PatientName");
+					String Email = rs.getString("Email");
+					String Phone = rs.getString("Phone");
+					String Password = rs.getString("Password"); 
+
+					// Add into the html table
+					output += "<tr><td><input id='hidPatientIDUpdate'name='hidPatientIDUpdate' type='hidden'value='" + PID + "'>" + PatientName + "</td>";output += "<td>" + Email + "</td>";output += "<td>" + Phone + "</td>";output += "<td>" + Password + "</td>";
+					// buttons
+					output += "<td><input name='btnUpdate' type='button'"+ "value='Update'"+"class='btnUpdate btn btn-secondary'></td>"+"<td><input name='btnRemove' type='button'"+" value='Remove'"+"class='btnRemove btn btn-danger' data-pid='"+ PID + "'>" + "</td></tr>";
+				}
+				con.close();
+				// Complete the html table
+				output += "</table>";
+			}
+			catch (Exception e)
+			{
+				output = "Error while reading the Patient.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
 
 }
