ca818a1f2eb0b32ab7e8b62edef86fddb1e1fcbb
HarshikaK <harshikakaushani21@gmil.com>
2020-05-05 23:40:15 +0530

    DB connection made

--- PAF_PatientService/src/com/Patient.java
+++ PAF_PatientService/src/com/Patient.java
@@ -1,5 +1,24 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Patient {
+	
+	//A common method to connect to the DB
+		private Connection connect()
+		{
+			Connection con = null;
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+
+				//Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/PatientService_db?useTimezone=true&serverTimezone=UTC", "root", "");
+			}
+			catch (Exception e)
+			{e.printStackTrace();}
+			return con;
+		} 
 
 }
